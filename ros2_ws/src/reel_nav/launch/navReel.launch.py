8aa343c79c4bb74a8474aca1b29bfbb67bfcfdab
Akifdlk <86167391+akif-dlk@users.noreply.github.com>
2025-07-23 01:03:46 +0300

    Update navReel.launch.py

--- ros2_ws/src/reel_nav/launch/navReel.launch.py
+++ ros2_ws/src/reel_nav/launch/navReel.launch.py
@@ -23,7 +23,7 @@ def generate_launch_description():
     slam_params_file = os.path.join(src_dir, "src", "reel_nav", "config", "slam_toolbox_params.yaml")  # SLAM yapılandırması
     map_dir = LaunchConfiguration(
         'map',
-        default=os.path.join(src_dir,"src","reel_nav",'map', 'otopark.yaml')
+        default=os.path.join(src_dir,"src","reel_nav",'map', 'bahce.yaml')
     )
     param_dir = LaunchConfiguration(
         'params_file',
