6f6952b30c12b810b3a1148311d48cfe491f867a
Oğuzcan kurt <99121324+oguzcankurt@users.noreply.github.com>
2025-08-08 09:18:27 +0300

    teknik test için

--- ros2_ws/src/reel_evata/setup.py
+++ ros2_ws/src/reel_evata/setup.py
@@ -40,6 +40,8 @@ setup(
             "sign_handler=" + package_name + ".sign_handler:main",
             "zedimu=" + package_name + ".zedimu:main",
             'movement = reel_evata.movement:main',
+            "teknikLaneD=" + package_name + ".teknikLaneD:main",	
+
 
         ],
     },
