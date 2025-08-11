8b934394574cacc5a757c3bbf14561da63fb2f5c
Akifdlk <86167391+akif-dlk@users.noreply.github.com>
2025-08-11 17:50:33 +0300

    durakta 15snye tek_control.py

--- ros2_ws/src/reel_evata/reel_evata/tek_control.py
+++ ros2_ws/src/reel_evata/reel_evata/tek_control.py
@@ -23,8 +23,10 @@ class FullMissionNode(Node):
         self.cmd_vel_pub = self.create_publisher(Twist, '/cmd_vel', 10)
         self.motor_power_pub = self.create_publisher(Int8, '/stm/motor_power', 10)
 
-        self.main_goals = [
-            {
+
+        # Ana hedefler
+        self.goals = [
+            {"type": "mini",
                 'x': 50.96047592163086,
                 'y': -11.834526062011719,
                 'z': 0.0,
@@ -33,7 +35,7 @@ class FullMissionNode(Node):
                 'oz': -0.09210611763136196,
                 'ow': 0.9957491968838729
             },
-            {
+            {"type": "mini",
                 'x': 59.73808288574219,
                 'y': -5.033398628234863,
                 'z': 0.0,
@@ -42,16 +44,7 @@ class FullMissionNode(Node):
                 'oz': 0.624960201794557,
                 'ow': 0.7806566122008489
             },
-            {
-                'x': 61.11116027832031,
-                'y': 5.986319541931152,
-                'z': 0.0,
-                'ox': 0.0,
-                'oy': 0.0,
-                'oz': 0.6240306447177314,
-                'ow': 0.7813998684752721
-            },   
-            {
+            {"type": "mini",
                 'x': 64.41436767578125,
                 'y': 22.521116256713867,
                 'z': 0.0,
@@ -60,7 +53,7 @@ class FullMissionNode(Node):
                 'oz': 0.6508404689866165,
                 'ow': 0.7592145177282117
             },            
-            {
+            {"type": "mini",
                 'x': 54.868648529052734,
                 'y': 32.496917724609375,
                 'z': 0.0,
@@ -69,7 +62,7 @@ class FullMissionNode(Node):
                 'oz': 0.9527808893412679,
                 'ow': 0.30365865195324593
             },
-            {
+            {"type": "mini",
                 'x': 46.75860595703125,
                 'y': 39.54705047607422,
                 'z': 0.0,
@@ -78,7 +71,7 @@ class FullMissionNode(Node):
                 'oz': -0.9931568476422821,
                 'ow': 0.11678816712854433
             },
-            {
+            {"type": "mini",
                 'x': 40.584407806396484,
                 'y': 33.28961944580078,
                 'z': 0.0,
@@ -87,7 +80,7 @@ class FullMissionNode(Node):
                 'oz': -0.6648541537368162,
                 'ow': 0.7469731951408309
             },
-            {
+            {"type": "mini",
                 'x': 41.75484085083008,
                 'y': 15.032739639282227,
                 'z': 0.0,
@@ -96,7 +89,7 @@ class FullMissionNode(Node):
                 'oz': -0.7664225249720035,
                 'ow': 0.6423367599752786
             },
-            {
+            {"type": "mini",
                 'x': 45.03278350830078,
                 'y': 6.588950157165527,
                 'z': 0.0,
@@ -105,7 +98,7 @@ class FullMissionNode(Node):
                 'oz': -0.08950049995705449,
                 'ow': 0.9959867772754
             },
-            {
+            {"type": "main",
                 'x': 54.20354461669922,
                 'y': 4.770875453948975,
                 'z': 0.0,
@@ -116,7 +109,7 @@ class FullMissionNode(Node):
             },
 
         ]
-        self.current_main_index = 0
+        self.current_index = 0
 
 
         self._in_diversion = False
@@ -125,7 +118,7 @@ class FullMissionNode(Node):
         self.processed_signs = set()
 
         self._active_goal_handle = None
-        self._current_main_goal_msg = None
+        self._current_goal_msg = None
         self.motion_enabled = True 
         self.mode = 'normal'
         self.original_goal = None
@@ -135,7 +128,7 @@ class FullMissionNode(Node):
         self.create_subscription(Odometry, '/odom', self.odom_callback, 10)
 
         self.get_logger().info(" Görev başlatılıyor...")
-        self.send_next_main_goal()
+        self.send_next_goal()
 
     def odom_callback(self, msg):
         pose = msg.pose.pose
@@ -145,47 +138,47 @@ class FullMissionNode(Node):
         self.current_yaw = yaw
         
 
-    def send_next_main_goal(self):
-        if self.current_main_index >= len(self.main_goals):
-            self.get_logger().info(" Tüm ana hedeflere ulaşıldı.")
+    def send_next_goal(self):
+        if self.current_index >= len(self.goals):
+            self.get_logger().info(" Tüm hedeflere ulaşıldı.")
             return
 
-        self.get_logger().info(f" Ana hedef {self.current_main_index + 1} gönderiliyor...")
-        goal_dict = self.main_goals[self.current_main_index]
+        goal_dict = self.goals[self.current_index]
+        self.get_logger().info(f" Hedef {self.current_index + 1} gönderiliyor...")
         goal_msg = self.create_pose_msg(goal_dict)
-        self._current_main_goal_msg = goal_msg
+        self._current_goal_msg = goal_msg
 
         if not self._action_client.wait_for_server(timeout_sec=3.0):
             self.get_logger().error("❌ Action server hazır değil.")
             return
 
         future = self._action_client.send_goal_async(goal_msg)
-        future.add_done_callback(self.main_goal_response_callback)
+        future.add_done_callback(self.goal_response_callback)
 
-    def main_goal_response_callback(self, future):
+    def goal_response_callback(self, future):
         goal_handle = future.result()
         if not goal_handle.accepted:
-            self.get_logger().error(" Ana hedef reddedildi.")
+            self.get_logger().error(" Hedef reddedildi.")
             return
 
-        self.get_logger().info("✅ Ana hedef kabul edildi.")
+        self.get_logger().info("✅ Hedef kabul edildi.")
         self._active_goal_handle = goal_handle
         result_future = goal_handle.get_result_async()
-        result_future.add_done_callback(self.main_goal_result_callback)
+        result_future.add_done_callback(self.goal_result_callback)
     
-    def main_goal_result_callback(self, future):
+    def goal_result_callback(self, future):
         try:
             result = future.result().result
-            self.get_logger().info("✅ Ana hedef tamamlandı.")
+            self.get_logger().info("Hedef tamamlandı.")
         except Exception as e:
-            self.get_logger().error(f"❌ Ana hedef tamamlanırken hata: {e}")
+            self.get_logger().error(f"❌ Hedef tamamlanırken hata: {e}")
 
 
     def check_goal_distance(self):
-        if not self.current_pose or self.current_main_index >= len(self.main_goals):
+        if not self.current_pose or self.current_index >= len(self.goals):
             return
 
-        target = self.main_goals[self.current_main_index]
+        target = self.goals[self.current_index]
         dist = math.hypot(
             target['x'] - self.current_pose.x,
             target['y'] - self.current_pose.y
@@ -195,16 +188,19 @@ class FullMissionNode(Node):
             self.get_logger().info(f"📍 Hedefe ulaşıldı ({dist:.2f} m). Sonraki hedefe geçiliyor...")
             if self._active_goal_handle:
                 cancel_future = self._active_goal_handle.cancel_goal_async()
-                cancel_future.add_done_callback(self._handle_goal_cancel)
+                cancel_future.add_done_callback(lambda fut: self._handle_goal_cancel(target))
             else:
-                self._proceed_to_next_goal()
+                self._handle_goal_cancel(target)
 
-    def _handle_goal_cancel(self, future):
+    def _handle_goal_cancel(self, target):
+        if target["type"] == "main":
+            self.get_logger().info("⏳ Ana hedefte 15 saniye bekleniyor...")
+            time.sleep(15)
         self._proceed_to_next_goal()
 
     def _proceed_to_next_goal(self):
-        self.current_main_index += 1
-        self.send_next_main_goal()
+        self.current_index += 1
+        self.send_next_goal()
 
     def create_pose_msg(self, pose_dict):
         msg = NavigateToPose.Goal()
